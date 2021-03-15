e2ef84650d7f56b7ff8e726237c2a9c4c926eae3
Craig5117 <craig.bennett5117@gmail.com>
2021-03-15 00:11:06 -0400

    moved wildcard out of prod

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
-import WrongPage from './components/WrongPage';
 
 
 
